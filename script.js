5d8978e500be3275242d062ebf41781254601b0f
Edouard <ntsekamedouard@gmail.com>
2026-05-05 21:37:43 +0100

    feat: add random button

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hey");
+
+const randomNumberEl = document.querySelector("#random-number")
+randomNumberEl.addEventListener("click", () =>{
+    document.querySelector("#result").textContent = Math.random();
+})
